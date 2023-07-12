22251b0aabffc70e616a4de922be55c2f28a578e
dotaplayer1 <georgemao72@gmail.com>
2023-07-12 11:15:39 -0700

    Change PUT /api/blogs/:id controller

--- controller/blogs.js
+++ controller/blogs.js
@@ -39,18 +39,21 @@ blogsRouter.delete("/:id", middleware.userExtractor, async (request, response) =
   response.status(204).end()
 })
 
-blogsRouter.put("/:id", async (request, response) => {
-  const { title, author, url, likes } = request.body
+blogsRouter.put("/:id", middleware.userExtractor, async (request, response) => {
+  const { title, author, url, likes, user } = request.body
 
   updatedBlog = await Blog.findByIdAndUpdate(
     request.params.id,
-    { title, author, url, likes },
+    { title, author, url, likes, user },
     { new: true, runValidators: true, context: "query" }
   )
 
-  updatedBlog === null
-    ? response.status(404).json({ error: "Blog does not exist" })
-    : response.json(updatedBlog)
+  if (updatedBlog === null) {
+    return response.status(404).json({ error: "Blog does not exist" })
+  }
+
+  await updatedBlog.populate("user", { username: 1, name: 1 })
+  response.json(updatedBlog)
 })
 
 module.exports = blogsRouter
